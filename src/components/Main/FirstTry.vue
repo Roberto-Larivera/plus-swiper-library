64bc392103a2d8413e7d1bb445592d53cc435758
Roberto Larivera <robylariv@gmail.com>
2023-02-27 18:33:35 +0100

    Complete FirstTry

--- src/components/Main/FirstTry.vue
+++ src/components/Main/FirstTry.vue
@@ -1,20 +1,84 @@
 <script>
+
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
 export default {
   name: 'FirstTry',
-  data(){
-    return{
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {};
+  },
+  data() {
+    return {
 
     }
-  }
-  
+  },
+
+
 }
 </script>
 
 <template>
-  <div>
-    FirstTry
+  <div class="container p-5">
+    <div class="row">
+      <div class="col text-center">
+        <h1>
+          Simple Carousel
+        </h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <swiper class="mySwiper">
+          <swiper-slide class="w-100 h-100">
+            <img src="https://picsum.photos/600/300" alt="">
+          </swiper-slide>
+          <swiper-slide class="w-100 h-100">
+            <img src="https://picsum.photos/600/300" alt="">
+          </swiper-slide>
+          <swiper-slide class="w-100 h-100">
+            <img src="https://picsum.photos/600/300" alt="">
+          </swiper-slide>
+          <swiper-slide class="w-100 h-100">
+            <img src="https://picsum.photos/600/300" alt="">
+          </swiper-slide>
+          <swiper-slide class="w-100 h-100">
+            <img src="https://picsum.photos/600/300" alt="">
+          </swiper-slide>
+          <swiper-slide class="w-100 h-100">
+            <img src="https://picsum.photos/600/300" alt="">
+          </swiper-slide>
+          <swiper-slide class="w-100 h-100">
+            <img src="https://picsum.photos/600/300" alt="">
+          </swiper-slide>
+          <swiper-slide class="w-100 h-100">
+            <img src="https://picsum.photos/600/300" alt="">
+          </swiper-slide>
+          <swiper-slide class="w-100 h-100">
+            <img src="https://picsum.photos/600/300" alt="">
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style lang="scss" scoped>
+.mySwiper {
+  background-color: lightcoral;
+  width: 600px;
+  height: 300px;
+}
+
+img {
+  height: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,4 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-  // import Swiper bundle with all modules installed
-  import Swiper from 'swiper/bundle';
-
-  // import styles bundle
-  import 'swiper/css/bundle';
-
-  // init Swiper:
-  //const swiper = new Swiper(...);
+   
